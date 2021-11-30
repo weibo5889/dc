ab4864590d9f9878ac5d98cab754210979fee7c7
weibo <weibo5@gmail.com>
2021-11-30 18:20:13 +0800

    更新

--- main.py
+++ main.py
@@ -39,24 +39,25 @@ async def on_message(message):
         if m == '!圖片':
             random_number = r.randrange(1, 7)
             await message.channel.send(file = discord.File(os.path.join(Config.PICDIR, f'pc-{random_number}.jpg')))
-        elif m == '!胡爾芳圖片':
+        if m == '!胡爾芳圖片':
             await message.channel.send(file=discord.File(os.path.join(Config.PICDIR, 'the_hu.jpg')))
-        elif m == '!憨包圖片':
+        if m == '!憨包圖片':
             await message.channel.send(file=discord.File(os.path.join(Config.PICDIR, 'nice.jpg')))
-        elif m == '!yoga圖片':
+        if m == '!yoga圖片':
             await message.channel.send(file=discord.File(os.path.join(Config.PICDIR, 'the_8.jpg')))
-        elif m == '!高好文圖片':
+        if m == '!高好文圖片':
             await message.channel.send(file=discord.File(os.path.join(Config.PICDIR, 'w.png')))
-        elif m == '!狗狗圖片':
+        if m == '!狗狗圖片':
             for i in range(1, 3):
                 await message.channel.send(file=discord.File(os.path.join(Config.PICDIR, f'dog-{i}.jpg')))
-
-    else:
-        send_message(m = m, message = message)
-        
     if m =="!抽籤":
         j = r.randrange(1,11)
         await message.channel.send(k[j])
+
+    else:
+        send_message(m = m, message = message)
+
+    
 
 if __name__ == 'main':
     keep_alive()
